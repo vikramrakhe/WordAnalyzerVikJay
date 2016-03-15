4607ccb59ed8b1b173c0a069ae00df07a31dd9b7
unknown <d1xayx@PNI6W11090.net.plm.eds.com>
2016-03-15 21:06:01 +0530

    Implementation with loops.

--- WordAnalyzer/Analyzer.cs
+++ WordAnalyzer/Analyzer.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
-
+using System.Threading;
 
 namespace WordAnalyzer
 {
@@ -11,37 +12,61 @@ namespace WordAnalyzer
         /// </summary>
         /// <param name="input"></param>
         public AnalysisResults Analyze(IEnumerable<string> input)
-        {           
-            var listInput = input.ToList();   
+        {
+            m_IWordCount = 0;
+            m_ILongestWordLength = 0;
+            m_MaxWordList = new List<string>();
 
-            m_lstMaxWordOccurance = null;
-            m_iwordCount = 0;
-            m_iLongestWordInList = 0;
-
-            //Total Word count in the list
-            m_iwordCount = listInput.Any()? listInput.Count() : 0 ; 
-
-            //Longest length word in the collection
-            m_iLongestWordInList = listInput.Any()? listInput.Select(inputl => inputl.Length).Max() : 0; 
-
-            // To Find the list of Max Occucarance of the word
-            var results = from word in listInput
-                            group word by word into newword
-                            select newword;
-            
-            var finalwordlist = results.Any() ? results.Where(x => x.Count() == results.Max(c => c.Count())).Select(a => a.Key) : null;
-            m_lstMaxWordOccurance = finalwordlist;            
-            
-            return new AnalysisResults()
+            foreach (var val in input)
             {
-                WordCount = m_iwordCount,
-                LongestWordLength = m_iLongestWordInList,
-                MostCommonlyUsedWords = m_lstMaxWordOccurance
-            };                
+                //System.Console.WriteLine(val);
+
+                //Total World Count
+                m_IWordCount++;
+
+                //Longest word in the collection.
+                if (val.Length > m_ILongestWordLength)
+                    m_ILongestWordLength = val.Length;
+
+                //Most occurance of the word
+                if (m_MaxOccurenceDictionary.ContainsKey(val))
+                {
+                    m_MaxOccurenceDictionary[val] = m_MaxOccurenceDictionary[val] + 1;
+                }
+                else
+                {
+                    m_MaxOccurenceDictionary.Add(val, 1);
+                }
+
+                int iCount = 1;
+                //find max count from m_MaxOccurenceDictionary
+                foreach (var pair in m_MaxOccurenceDictionary)
+                {
+                    if (pair.Value > iCount)
+                    {
+                        m_MaxWordList.Clear();
+                        iCount = pair.Value;
+                        m_MaxWordList.Add(pair.Key);
+                    }
+                    else if (pair.Value == iCount)
+                    {
+                        m_MaxWordList.Add(pair.Key);
+                    }
+
+                }
+
+            }
+            return new AnalysisResults
+            {
+                LongestWordLength = m_ILongestWordLength,
+                WordCount = m_IWordCount,
+                MostCommonlyUsedWords = m_MaxWordList
+            };
         }
 
-        private int m_iwordCount;
-        private int m_iLongestWordInList;
-        private IEnumerable<string> m_lstMaxWordOccurance;
+        private int m_IWordCount;
+        private int m_ILongestWordLength;
+        private List<string> m_MaxWordList;
+        private readonly Dictionary<string, int> m_MaxOccurenceDictionary = new Dictionary<string, int>();
     }
 }
